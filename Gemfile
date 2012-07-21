b4b72aa7681c3e289b05232d3be0581542be59b6
Gustav Montgomerie <gusmon@gmail.com>
2012-07-21 20:47:14 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+gem 'faker'
+
+group :development, :test do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
+end
+
+group :development do 
+  gem 'rspec-rails', '2.11.0'
+end
+
+group :test do
+  gem 'webrat'
+  gem 'rspec'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiApp::Application.config.secret_token = 'a58e50966aeae46c76918f02840a8e0dbe167ef5f05f43d4d14f32d4bca66d75d6b9700f3e9f612519bf253d3535acb47a32ea12a3ffedaf2bbbd2e620cbd91d'
